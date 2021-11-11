c5893a5d6abf4a204aa92e6608d2750abcd91a3b
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2021-11-11 17:00:39 +0900

    fp number comparison in verification

--- common/my_verification.h
+++ common/my_verification.h
@@ -8,17 +8,31 @@
 #include <omp.h>
 #include <errno.h>
 
-#define MY_VERIFY_INT(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, int, 0)
-#define MY_VERIFY_RAW(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, char, 0)
-#define MY_VERIFY_CHAR(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, char, 0)
-#define MY_VERIFY_FLOAT_EXACT(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, float, 0.0)
-#define MY_VERIFY_DOUBLE_EXACT(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, double, 0.0)
+#define MY_VERIFY_INT(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, int, MY_INT_STYLE_EQ, 0, 0)
+#define MY_VERIFY_RAW(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, char, MY_INT_STYLE_EQ, 0, 0)
+#define MY_VERIFY_CHAR(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, char, MY_INT_STYLE_EQ, 0, 0)
+#define MY_VERIFY_FLOAT_EXACT(ARRAY_PTR, SIZE)                                \
+	MY_VERIFY(ARRAY_PTR, SIZE, float, MY_FP_STYLE_EQ, FLT_MIN, (FLT_EPSILON * 256), FLT_MAX)
+#define MY_VERIFY_DOUBLE_EXACT(ARRAY_PTR, SIZE)                               \
+	MY_VERIFY(ARRAY_PTR, SIZE, double, MY_FP_STYLE_EQ, DBL_MIN, (DBL_EPSILON * 256), DBL_MAX)
+//#define MY_VERIFY_FLOAT_EXACT(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, float, 0.0)
+//#define MY_VERIFY_DOUBLE_EXACT(ARRAY_PTR, SIZE) MY_VERIFY(ARRAY_PTR, SIZE, double, 0.0)
 
-#define S(x) #x
-#define S_(x) S(x)
-#define S__LINE__ S_(__LINE__)
+#define MY_S(x) #x
+#define MY_S_(x) MY_S(x)
+#define MY_S__LINE__ MY_S_(__LINE__)
 
-#define MY_VERIFY(ARRAY_PTR, SIZE, TYPE, EPSILON)                       \
+#define MY_MIN(X, Y) (((X) > (Y)) ? (Y) : (X))
+#define MY_MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+#define MY_ABS(X) ((X) > 0 ? (X) : -(X))
+
+#define MY_FP_STYLE_EQ(X, Y, ABS_TH, EPSILON, FP_MAX)                          \
+	(((X) == (Y)) || (MY_ABS((X) - (Y)) < MY_MAX((MY_ABS_TH), (EPSILON) * MY_MIN((MY_ABS(X) + MY_ABS(Y)), FP_MAX))))
+
+#define MY_INT_STYLE_EQ(X, Y, ABS_TH, EPSILON)                           \
+	((X) == (Y))
+
+#define MY_VERIFY(ARRAY_PTR, SIZE, TYPE, EQ, ABS_TH, EPSILON)            \
   do { \
     char *verification_dir = getenv("MY_VERIFICATION_DIR"); \
     if (verification_dir && strcmp(verification_dir, "")) { \
@@ -31,8 +45,8 @@
       const char *src_filename = strrchr(__FILE__, '/'); \
       if (!src_filename) \
         src_filename = __FILE__; \
-      char verification_id[strlen(src_filename) + strlen(S__LINE__) + 2]; \
-      sprintf(verification_id, "%s:%s", src_filename, S__LINE__); \
+      char verification_id[strlen(src_filename) + strlen(MY_S__LINE__) + 2]; \
+      sprintf(verification_id, "%s:%s", src_filename, MY_S__LINE__); \
       char verification_file[strlen(verification_id) + strlen(verification_dir) + 2]; \
       sprintf(verification_file, "%s/%s", verification_dir, verification_id); \
       if (getenv("MY_VERIFICATION_DUMP")) { \
@@ -55,8 +69,8 @@
         for (TYPE *el = (TYPE *) array, *correct = (TYPE *) data; \
              el < ((TYPE *) array) + size;                         \
              el++, correct++) {              \
-          if (!(*el - *correct <= eps && *correct - *el <= eps)) { \
-            fprintf(stderr, "Verification failed at %s:%s\n", __FILE__, S__LINE__); \
+          if (!EQ(*el, *correct, ABS_TH, EPSILON)) { \
+            fprintf(stderr, "Verification failed at %s:%s\n", __FILE__, MY_S__LINE__); \
             pass = 0; \
             if (halt_when_incorrect) { \
               fprintf(stderr, "Halting\n"); \
