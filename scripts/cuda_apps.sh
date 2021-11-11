19c5a563a541d8d486b995739a164908a255c576
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2021-11-11 17:03:25 +0900

    add cuda apps

---
+++ scripts/cuda_apps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -n backprop ""
+echo -n bfs ""
+echo -n b+tree ""
+echo -n cfd ""
+#echo -n dwt2d "" # cpucuda hangs
+echo -n gaussian ""
+echo -n heartwall ""
+echo -n hotspot ""
+echo -n hotspot3D ""
+#echo -n huffman "" # cpucuda hangs
+#echo -n hybridsort "" # cpucuda unable to compile
+#echo -n kmeans "" # cpucuda ubable to compile
+echo -n lavaMD ""
+echo -n lud ""
+#echo -n mummergpu "" # cpucuda unable to compile
+echo -n myocyte ""
+echo -n nn ""
+echo -n nw ""
+echo -n particlefilter ""
+echo -n pathfinder ""
+#echo -n srad/srad_v1 "" # cpucuda hangs
+echo -n srad/srad_v2 ""
+echo -n streamcluster ""
+echo
