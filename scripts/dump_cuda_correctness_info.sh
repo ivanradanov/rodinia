1665e5747790450461f8455b62114f8d03f1f2e3
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2021-11-11 10:53:56 +0900

    verification scripts

---
+++ scripts/dump_cuda_correctness_info.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export MY_VERIFICATION_DIR="$DIR/../verification_data/"
+export MY_VERIFICATION_DUMP=1
+
+mkdir -p "$MY_VERIFICATION_DIR"
+
+"$DIR/run_timed_common.sh" "cuda" "backprop bfs b+tree cfd dwt2d gaussian heartwall hotspot hotspot3D huffman lavaMD lud myocyte nn nw particlefilter pathfinder srad/srad_v1 srad/srad_v2 streamcluster"
