86ceffbe2389d67074641dba86de4689d9317630
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2021-12-06 17:41:43 +0900

    Fix wrong assignment from void* to TYPE*

--- common/my_verification.h
+++ common/my_verification.h
@@ -75,7 +75,7 @@
       size_t size = (SIZE); \
       size_t type_size = sizeof(TYPE); \
       size_t array_size = type_size * size; \
-      TYPE *array = (ARRAY_PTR); \
+      TYPE *array = (TYPE *)(ARRAY_PTR); \
       const char *src_filename = strrchr(__FILE__, '/'); \
       if (!src_filename) \
         src_filename = __FILE__; \
