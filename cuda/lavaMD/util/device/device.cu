ca93646899fd1fd0ba1fac5d59ae3a380ee60cff
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-01-27 17:47:17 +0900

    comment out cuda error exits in lavaMD

--- cuda/lavaMD/util/device/device.cu
+++ cuda/lavaMD/util/device/device.cu
@@ -58,7 +58,7 @@ void checkCUDAError(const char *msg)
 		// fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString( err) );
 		printf("Cuda error: %s: %s.\n", msg, cudaGetErrorString( err) );
 		fflush(NULL);
-		exit(EXIT_FAILURE);
+		//exit(EXIT_FAILURE);
 	}
 }
 
