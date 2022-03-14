4251611afd686090e61e9d28e08cd4472adf2369
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-03-14 12:50:41 +0900

    Add copy binaries script

---
+++ scripts/copy_binaries.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+then
+    echo usage: copy_binaries.sh root/of/rodinia/tree
+    exit 1
+fi
+
+TARGET="$1"
+
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+BM="$($DIR/cuda_apps.sh)"
+
+SUBDIR=cuda
+RODINIA_ROOT_DIR="$DIR/../"
+RODINIA_APP_DIR="$RODINIA_ROOT_DIR/$SUBDIR/"
+
+for b in $BM; do
+    echo copying "$b" binaries...
+    cd "$RODINIA_APP_DIR/$b"
+
+    BINARIES=$(cat run | awk '!/^ *#/ && NF' | awk '{print $1}' | sort | uniq)
+
+    echo $BINARIES
+    for BIN in $BINARIES; do
+        DIRNAME=$(dirname $BIN)
+        ACTUAL_TARGET="$TARGET/$SUBDIR/$b/$DIRNAME"
+        echo $BIN "->" $ACTUAL_TARGET
+        rsync -Pa $BIN $ACTUAL_TARGET
+    done
+
+done
