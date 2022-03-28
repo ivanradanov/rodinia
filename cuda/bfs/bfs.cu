377501a5698d8b2e23a2a01ed0798bfbeafcea4d
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-03-28 12:44:02 +0900

    fix verification bfs

--- cuda/bfs/bfs.cu
+++ cuda/bfs/bfs.cu
@@ -258,6 +258,9 @@ void BFSGraph( int argc, char** argv)
 	d2h_time += tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
 #endif
 
+
+
+
 	MY_VERIFY_INT(h_cost, no_of_nodes);
 
 	//Store the result into a file
