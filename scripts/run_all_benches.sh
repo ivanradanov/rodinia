13a6e9df61e06fff24832e6efb6f35f950ab5903
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-04-15 07:20:08 +0900

    update bench script

--- scripts/run_all_benches.sh
+++ scripts/run_all_benches.sh
@@ -1,60 +1,73 @@
 #!/usr/bin/env bash
 
-set -x
+HOST=memkf02
 
-NRUNS=10
+NRUNS=5
+NRUNS_SCALING=2
 
 #DATE="$(date -Ins)"
 #LOG_FILE="~/tmp/all_benches_log_$DATE.log"
 
-MAX_THREAD_TEST_CONFIGS="1 2 3 6 7 8"
-SCALING_TEST_CONFIGS="2 3"
+MAX_THREAD_TEST_CONFIGS="1 2 3 6 7 8 9"
+SCALING_TEST_CONFIGS="2 3 9"
 THREAD_NUMS="1 2 4 8 16"
-THREAD_NUMS_OPENMP="1 2 4 8 16 24"
-#THREAD_NUMS_OPENMP="24"
+THREAD_NUMS_OPENMP="1 2 4 8 16"
 
+while true
+do
+
+echo ----------------------------------------------
 echo Start "$(date -Ins)"
+echo ----------------------------------------------
 
-./scripts/enable-config.sh common/host.make.config common/memkf02.polygeist.host.make.config
+./scripts/enable-config.sh common/host.make.config common/$HOST.polygeist.host.make.config
 
 echo Max thread tests
 for i in $MAX_THREAD_TEST_CONFIGS; do
-    echo Compiling configuration $i...
-    make cuda_clean &> /dev/null
-    make CONFIG="$i" MY_VERIFICATION_DISABLE=1 cuda &> /dev/null
+	echo Compiling configuration $i...
+	make cuda_clean &> /dev/null
+	make CONFIG="$i" MY_VERIFICATION_DISABLE=1 cuda &> /dev/null
 
-    echo Running configuration $i...
-    ./scripts/run_timed_cuda_n_times.sh $NRUNS
+	echo Running configuration $i...
+	./scripts/run_timed_cuda_n_times.sh $NRUNS | grep -B1 FAIL
 
 done
 
 echo Scaling tests cuda
 for i in $SCALING_TEST_CONFIGS; do
-    echo Compiling configuration $i...
-    make cuda_clean &> /dev/null
-    make CONFIG="$i" MY_VERIFICATION_DISABLE=1 cuda &> /dev/null
+	echo Compiling configuration $i...
+	make cuda_clean &> /dev/null
+	make CONFIG="$i" MY_VERIFICATION_DISABLE=1 cuda &> /dev/null
 
-    echo Running configuration $i...
-    ./scripts/run_scaling_test.sh "./scripts/run_timed_cuda_n_times.sh $NRUNS" $THREAD_NUMS
+	echo Running configuration $i...
+	./scripts/run_scaling_test.sh "./scripts/run_timed_cuda_n_times.sh $NRUNS_SCALING" $THREAD_NUMS | grep -B1 FAIL
 done
 
 echo Finished cuda "$(date -Ins)"
 
 echo Scaling tests openmp
 
-./scripts/enable-config.sh common/openmp.host.make.config common/memkf02.polygeist-clang.openmp.host.make.config
+./scripts/enable-config.sh common/openmp.host.make.config common/$HOST.polygeist-clang.openmp.host.make.config
 make omp_clean &> /dev/null
 make omp &> /dev/null
-./scripts/run_scaling_test.sh "./scripts/run_timed_openmp_n_times.sh $NRUNS" $THREAD_NUMS_OPENMP
+./scripts/run_timed_openmp_n_times.sh $NRUNS | grep -B1 FAIL
+./scripts/run_scaling_test.sh "./scripts/run_timed_openmp_n_times.sh $NRUNS_SCALING" $THREAD_NUMS_OPENMP | grep -B1 FAIL
 
-./scripts/enable-config.sh common/openmp.host.make.config common/memkf02.polygeist.openmp.host.make.config
+./scripts/enable-config.sh common/openmp.host.make.config common/$HOST.polygeist.openmp.host.make.config
 make omp_clean &> /dev/null
 make omp &> /dev/null
-./scripts/run_scaling_test.sh "./scripts/run_timed_openmp_n_times.sh $NRUNS" $THREAD_NUMS_OPENMP
+./scripts/run_timed_openmp_n_times.sh $NRUNS | grep -B1 FAIL
+./scripts/run_scaling_test.sh "./scripts/run_timed_openmp_n_times.sh $NRUNS_SCALING" $THREAD_NUMS_OPENMP | grep -B1 FAIL
 
-./scripts/enable-config.sh common/openmp.host.make.config "common/memkf02.polygeist.openmp.openmp-opt=0.host.make.config"
+./scripts/enable-config.sh common/openmp.host.make.config "common/$HOST.polygeist.openmp.openmp-opt=0.host.make.config"
 make omp_clean &> /dev/null
 make omp &> /dev/null
-./scripts/run_scaling_test.sh "./scripts/run_timed_openmp_n_times.sh $NRUNS" $THREAD_NUMS_OPENMP
+#./scripts/run_scaling_test.sh "./scripts/run_timed_openmp_n_times.sh $NRUNS_SCALING" $THREAD_NUMS_OPENMP | grep -B1 FAIL
+./scripts/run_timed_openmp_n_times.sh $NRUNS | grep -B1 FAIL
 
+echo COPYING RESULTS, DONT QUIT
+rsync -a --remove-source-files results ~/rodinia_results/
 echo Finished "$(date -Ins)"
+
+done
+
