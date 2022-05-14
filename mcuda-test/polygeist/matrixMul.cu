025fa7dc6929d44f02bcbc9d9418c0472c6a124e
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-05-14 14:30:44 +0900

    Remove irrelevant parts from mcuda comparison code

--- mcuda-test/polygeist/matrixMul.cu
+++ mcuda-test/polygeist/matrixMul.cu
@@ -240,33 +240,6 @@ int MatrixMultiply(int argc, char **argv, int block_size, const dim3 &dimsA,
   printf("%f,", msecPerMatrixMul / 1000.0f);
 
 
-  // Copy result from device to host
-  checkCudaErrors(
-      cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost));
-  //checkCudaErrors(cudaStreamSynchronize(stream));
-
-  //printf("Checking computed result for correctness: ");
-  bool correct = true;
-
-  // test relative error by the formula
-  //     |<x, y>_cpu - <x,y>_gpu|/<|x|, |y|>  < eps
-  double eps = 1.e-6;  // machine zero
-
-  for (int i = 0; i < static_cast<int>(dimsC.x * dimsC.y); i++) {
-    double abs_err = fabs(h_C[i] - (dimsA.x * valB));
-    double dot_length = dimsA.x;
-    double abs_val = fabs(h_C[i]);
-    double rel_err = abs_err / abs_val / dot_length;
-
-    if (rel_err > eps) {
-      printf("Error! Matrix[%05d]=%.8f, ref=%.8f error term is > %E\n", i,
-             h_C[i], dimsA.x * valB, eps);
-      correct = false;
-    }
-  }
-
-  //printf("%s\n", correct ? "Result = PASS" : "Result = FAIL");
-
   // Clean up memory
   checkCudaErrors(cudaFreeHost(h_A));
   checkCudaErrors(cudaFreeHost(h_B));
@@ -279,12 +252,7 @@ int MatrixMultiply(int argc, char **argv, int block_size, const dim3 &dimsA,
   //printf(
   //"\nNOTE: The CUDA Samples are not meant for performance"
   //"measurements. Results may vary when GPU Boost is enabled.\n");
-
-  if (correct) {
-    return EXIT_SUCCESS;
-  } else {
-    return EXIT_FAILURE;
-  }
+  return EXIT_SUCCESS;
 }
 
 /**
