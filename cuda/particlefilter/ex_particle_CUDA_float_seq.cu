e48d8e74a9dd1df4bd14aa25c40e803ec90740d7
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-08-03 21:14:05 +0900

    WIP making particlefilter more predictable

--- cuda/particlefilter/ex_particle_CUDA_float_seq.cu
+++ cuda/particlefilter/ex_particle_CUDA_float_seq.cu
@@ -107,6 +107,7 @@ __device__ void cdfCalc(double * CDF, double * weights, int Nparticles) {
     CDF[0] = weights[0];
     for (x = 1; x < Nparticles; x++) {
         CDF[x] = weights[x] + CDF[x - 1];
+        printf("CDF[%d] = %f + %f = %f\n", x, weights[x], CDF[x-1], CDF[x]);
     }
 }
 
@@ -116,6 +117,7 @@ __device__ void cdfCalc(double * CDF, double * weights, int Nparticles) {
  * returns a double representing a randomily generated number from a uniform distribution with range [0, 1)
  ******************************/
 __device__ double d_randu(int * seed, int index) {
+  return (1000 + index) / 5000000;
 
     int M = INT_MAX;
     int A = 1103515245;
@@ -134,6 +136,7 @@ __device__ double d_randu(int * seed, int index) {
 */
 
 double randu(int * seed, int index) {
+  return (1000 + index) / 5000000;
     int num = A * seed[index] + C;
     seed[index] = num % M;
     return fabs(seed[index] / ((double) M));
@@ -148,6 +151,7 @@ double randu(int * seed, int index) {
  * @see http://en.wikipedia.org/wiki/Normal_distribution, section computing value for normal random distribution
  */
 double randn(int * seed, int index) {
+  return (1000 + index) / 5000000;
     /*Box-Muller algorithm*/
     double u = randu(seed, index);
     double v = randu(seed, index);
@@ -157,6 +161,7 @@ double randn(int * seed, int index) {
 }
 
 double test_randn(int * seed, int index) {
+  return (1000 + index) / 5000000;
     //Box-Muller algortihm
     double pi = 3.14159265358979323846;
     double u = randu(seed, index);
@@ -167,6 +172,7 @@ double test_randn(int * seed, int index) {
 }
 
 __device__ double d_randn(int * seed, int index) {
+  return (1000 + index) / 5000000;
     //Box-Muller algortihm
     double pi = 3.14159265358979323846;
     double u = d_randu(seed, index);
@@ -257,6 +263,8 @@ __global__ void find_index_kernel(double * arrayX, double * arrayY, double * CDF
                 break;
             }
         }
+	printf("index = %d\n", index);
+
         if (index == -1) {
             index = Nparticles - 1;
         }
@@ -264,7 +272,7 @@ __global__ void find_index_kernel(double * arrayX, double * arrayY, double * CDF
         xj[i] = arrayX[index];
         yj[i] = arrayY[index];
 
-        //weights[i] = 1 / ((double) (Nparticles)); //moved this code to the beginning of likelihood kernel
+        weights[i] = 1 / ((double) (Nparticles)); //moved this code to the beginning of likelihood kernel
 
     }
     __syncthreads();
@@ -275,18 +283,22 @@ __global__ void normalize_weights_kernel(double * weights, int Nparticles, doubl
     int i = blockDim.x * block_id + threadIdx.x;
     __shared__ double u1, sumWeights;
     
-    if(0 == threadIdx.x)
+    if(0 == threadIdx.x) {
         sumWeights = partial_sums[0];
+    }
     
     __syncthreads();
     
     if (i < Nparticles) {
         weights[i] = weights[i] / sumWeights;
+        printf("HMM weights[%d] = %f / %f\n", i, weights[i], sumWeights);
     }
     
     __syncthreads(); 
     
     if (i == 0) {
+      printf("weights[0] = %f\n", weights[0]);
+      printf("weights[1] = %f\n", weights[1]);
         cdfCalc(CDF, weights, Nparticles);
         u[0] = (1 / ((double) (Nparticles))) * d_randu(seed, i); // do this to allow all threads in all blocks to use the same u1
     }
@@ -347,7 +359,7 @@ __global__ void likelihood_kernel(double * arrayX, double * arrayY, double * xj,
         arrayX[i] = xj[i]; 
         arrayY[i] = yj[i]; 
 
-        weights[i] = 1 / ((double) (Nparticles)); //Donnie - moved this line from end of find_index_kernel to prevent all weights from being reset before calculating position on final iteration.
+        //weights[i] = 1 / ((double) (Nparticles)); //Donnie - moved this line from end of find_index_kernel to prevent all weights from being reset before calculating position on final iteration.
 
         arrayX[i] = arrayX[i] + 1.0 + 5.0 * d_randn(seed, i);
         arrayY[i] = arrayY[i] - 2.0 + 2.0 * d_randn(seed, i);
@@ -371,7 +383,7 @@ __global__ void likelihood_kernel(double * arrayX, double * arrayY, double * xj,
         likelihood[i] = likelihood[i] / countOnes;
         
         weights[i] = weights[i] * exp(likelihood[i]); //Donnie Newell - added the missing exponential function call
-        
+
     }
 
     buffer[threadIdx.x] = 0.0;
@@ -739,9 +751,33 @@ void particleFilter(unsigned char * I, int IszX, int IszY, int Nfr, int * seed, 
         likelihood_kernel <<< num_blocks, threads_per_block >>> (arrayX_GPU, arrayY_GPU, xj_GPU, yj_GPU, CDF_GPU, ind_GPU, objxy_GPU, likelihood_GPU, I_GPU, u_GPU, weights_GPU, Nparticles, countOnes, max_size, k, IszY, Nfr, seed_GPU, partial_sums);
 
         sum_kernel <<< num_blocks, threads_per_block >>> (partial_sums, Nparticles);
+                for (int i = 0; i < Nparticles; i++) {
+        #define p(A) printf(#A "[%d] = %f\n", i, A[i])
+                p(arrayX_GPU);
+                p(arrayY_GPU);
+                p(xj_GPU);
+                p(yj_GPU);
+                p(CDF_GPU);
+                p(weights_GPU);
+                p(likelihood_GPU);
+                p(u_GPU);
+              }
 
         normalize_weights_kernel <<< num_blocks, threads_per_block >>> (weights_GPU, Nparticles, partial_sums, CDF_GPU, u_GPU, seed_GPU);
-        
+        printf("normalize\n");
+                printf("sumWeights = %f\n", partial_sums[0]);
+                for (int i = 0; i < Nparticles; i++) {
+        #define p(A) printf(#A "[%d] = %f\n", i, A[i])
+                p(arrayX_GPU);
+                p(arrayY_GPU);
+                p(xj_GPU);
+                p(yj_GPU);
+                p(CDF_GPU);
+                p(weights_GPU);
+                p(likelihood_GPU);
+                p(u_GPU);
+              }
+
         find_index_kernel <<< num_blocks, threads_per_block >>> (arrayX_GPU, arrayY_GPU, CDF_GPU, u_GPU, xj_GPU, yj_GPU, weights_GPU, Nparticles);
 
     }//end loop
@@ -869,11 +905,10 @@ int main(int argc, char * argv[]) {
         return 0;
     }
     //establish seed
-    srand(7);
     int * seed = (int *) malloc(sizeof (int) *Nparticles);
     int i;
     for (i = 0; i < Nparticles; i++)
-	    seed[i] = rand() * i;
+	    seed[i] = i;
     //malloc matrix
     unsigned char * I = (unsigned char *) malloc(sizeof (unsigned char) *IszX * IszY * Nfr);
     long long start = get_time();
