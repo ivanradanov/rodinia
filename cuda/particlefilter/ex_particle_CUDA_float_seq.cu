0fc3ad2ead21bdc6798ba9613cc1425a195590a6
William S. Moses <gh@wsmoses.com>
2022-08-15 19:54:49 -0400

    min struggle

--- cuda/particlefilter/ex_particle_CUDA_float_seq.cu
+++ cuda/particlefilter/ex_particle_CUDA_float_seq.cu
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NN 200
+#define NN 100
 
 #define STRINGIZE_NX(A) #A
 #define STRINGIZE(A) STRINGIZE_NX(A)
 
 #define NNS STRINGIZE(NN)
-#define threads_per_block 2
+#define threads_per_block 1
 
-#define check_error(X) X
 
 __global__ void normalize_weights_kernel1(double * weights, int Nparticles) {
     int block_id = blockIdx.x;
     int i = blockDim.x * block_id + threadIdx.x;
     __shared__ double sumWeights;
+    //printf("i=%d sw=%p\n", i, &sumWeights);
     
     if(0 == threadIdx.x) {
         sumWeights = 1 / ((double) (Nparticles));
@@ -22,9 +22,7 @@ __global__ void normalize_weights_kernel1(double * weights, int Nparticles) {
     
     __syncthreads();
     
-    if (i < Nparticles) {
-        weights[i] = 1 / ((double) (Nparticles)) / sumWeights;
-    }
+    weights[i] = 1 / ((double) (Nparticles)) / sumWeights;
 }
 
 
@@ -44,9 +42,7 @@ __global__ void normalize_weights_kernel2(double * u, int Nparticles) {
     
     __syncthreads();
         
-    if (i < Nparticles) {
-        u[i] = u1 + i / ((double) (Nparticles));
-    }
+    u[i] = u1 + i / ((double) (Nparticles));
 }
 
 int main() {
@@ -54,11 +50,6 @@ int main() {
     int Nparticles = atoi(NNS);
     
     int x, y;
-    //initial weights are all equal (1/Nparticles)
-    double * weights = (double *) malloc(sizeof (double) *Nparticles);
-    for (x = 0; x < Nparticles; x++) {
-        weights[x] = 1 / ((double) (Nparticles));
-    }
 
     //GPU copies of arrays
     double * weights_GPU;
@@ -66,13 +57,11 @@ int main() {
     double * u_GPU;
 
     //CUDA memory allocation
-    check_error(cudaMalloc((void **) &u_GPU, sizeof (double) *Nparticles));
-    check_error(cudaMalloc((void **) &weights_GPU, sizeof (double) *Nparticles));
+    cudaMalloc((void **) &u_GPU, sizeof (double) *Nparticles);
+    cudaMalloc((void **) &weights_GPU, sizeof (double) *Nparticles);
 
 
     int indX, indY;
-    //start send
-    check_error(cudaMemcpy(weights_GPU, weights, sizeof (double) *Nparticles, cudaMemcpyHostToDevice));
     int num_blocks = ( Nparticles + threads_per_block - 1) / threads_per_block;
     printf("nb=%d nn=%d\n", num_blocks, num_blocks * threads_per_block);
 
@@ -103,6 +92,5 @@ int main() {
 
     //CUDA freeing of memory
     cudaFree(weights_GPU);
-    free(weights);
     return 0;
 }
