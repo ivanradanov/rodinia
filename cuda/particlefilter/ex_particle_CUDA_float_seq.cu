71fd1684ba27b92ee26c203b4b58c5120c919e9c
William S. Moses <gh@wsmoses.com>
2022-08-15 17:54:05 -0400

    simpl

--- cuda/particlefilter/ex_particle_CUDA_float_seq.cu
+++ cuda/particlefilter/ex_particle_CUDA_float_seq.cu
@@ -19,23 +19,7 @@
 
 const int threads_per_block = 512;
 
-/**
-@var M value for Linear Congruential Generator (LCG); use GCC's value
- */
-long M = INT_MAX;
-/**
-@var A value for LCG
- */
-int A = 1103515245;
-/**
-@var C value for LCG
- */
-int C = 12345;
 
-/*****************************
- *GET_TIME
- *returns a long int representing the time
- *****************************/
 long long get_time() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -94,49 +78,6 @@ __device__ double d_randu(int * seed, int index) {
     seed[index] = num % M;
 
     return fabs(seed[index] / ((double) M));
-}/**
-* Generates a uniformly distributed random number using the provided seed and GCC's settings for the Linear Congruential Generator (LCG)
-* @see http://en.wikipedia.org/wiki/Linear_congruential_generator
-* @note This function is thread-safe
-* @param seed The seed array
-* @param index The specific index of the seed to be advanced
-* @return a uniformly distributed number [0, 1)
-*/
-
-double randu(int * seed, int index) {
-  return (1000 + index) / 5000000;
-    int num = A * seed[index] + C;
-    seed[index] = num % M;
-    return fabs(seed[index] / ((double) M));
-}
-
-/**
- * Generates a normally distributed random number using the Box-Muller transformation
- * @note This function is thread-safe
- * @param seed The seed array
- * @param index The specific index of the seed to be advanced
- * @return a double representing random number generated using the Box-Muller algorithm
- * @see http://en.wikipedia.org/wiki/Normal_distribution, section computing value for normal random distribution
- */
-double randn(int * seed, int index) {
-  return (1000 + index) / 5000000;
-    /*Box-Muller algorithm*/
-    double u = randu(seed, index);
-    double v = randu(seed, index);
-    double cosine = cos(2 * PI * v);
-    double rt = -2 * log(u);
-    return sqrt(rt) * cosine;
-}
-
-double test_randn(int * seed, int index) {
-  return (1000 + index) / 5000000;
-    //Box-Muller algortihm
-    double pi = 3.14159265358979323846;
-    double u = randu(seed, index);
-    double v = randu(seed, index);
-    double cosine = cos(2 * pi * v);
-    double rt = -2 * log(u);
-    return sqrt(rt) * cosine;
 }
 
 __device__ double d_randn(int * seed, int index) {
@@ -148,49 +89,6 @@ __device__ double d_randn(int * seed, int index) {
     double cosine = cos(2 * pi * v);
     double rt = -2 * log(u);
     return sqrt(rt) * cosine;
-}
-
-/****************************
-UPDATE WEIGHTS
-UPDATES WEIGHTS
-param1 weights
-param2 likelihood
-param3 Nparcitles
- ****************************/
-__device__ double updateWeights(double * weights, double * likelihood, int Nparticles) {
-    int x;
-    double sum = 0;
-    for (x = 0; x < Nparticles; x++) {
-        weights[x] = weights[x] * exp(likelihood[x]);
-        sum += weights[x];
-    }
-    return sum;
-}
-
-__device__ int findIndexBin(double * CDF, int beginIndex, int endIndex, double value) {
-    if (endIndex < beginIndex)
-        return -1;
-    int middleIndex;
-    while (endIndex > beginIndex) {
-        middleIndex = beginIndex + ((endIndex - beginIndex) / 2);
-        if (CDF[middleIndex] >= value) {
-            if (middleIndex == 0)
-                return middleIndex;
-            else if (CDF[middleIndex - 1] < value)
-                return middleIndex;
-            else if (CDF[middleIndex - 1] == value) {
-                while (CDF[middleIndex] == value && middleIndex >= 0)
-                    middleIndex--;
-                middleIndex++;
-                return middleIndex;
-            }
-        }
-        if (CDF[middleIndex] > value)
-            endIndex = middleIndex - 1;
-        else
-            beginIndex = middleIndex + 1;
-    }
-    return -1;
 }
 
 /** added this function. was missing in original double version.
@@ -379,46 +277,6 @@ double roundDouble(double value) {
 }
 
 /**
- * Set values of the 3D array to a newValue if that value is equal to the testValue
- * @param testValue The value to be replaced
- * @param newValue The value to replace testValue with
- * @param array3D The image vector
- * @param dimX The x dimension of the frame
- * @param dimY The y dimension of the frame
- * @param dimZ The number of frames
- */
-void setIf(int testValue, int newValue, unsigned char * array3D, int * dimX, int * dimY, int * dimZ) {
-    int x, y, z;
-    for (x = 0; x < *dimX; x++) {
-        for (y = 0; y < *dimY; y++) {
-            for (z = 0; z < *dimZ; z++) {
-                if (array3D[x * *dimY * *dimZ + y * *dimZ + z] == testValue)
-                    array3D[x * *dimY * *dimZ + y * *dimZ + z] = newValue;
-            }
-        }
-    }
-}
-
-/**
- * Sets values of 3D matrix using randomly generated numbers from a normal distribution
- * @param array3D The video to be modified
- * @param dimX The x dimension of the frame
- * @param dimY The y dimension of the frame
- * @param dimZ The number of frames
- * @param seed The seed array
- */
-void addNoise(unsigned char * array3D, int * dimX, int * dimY, int * dimZ, int * seed) {
-    int x, y, z;
-    for (x = 0; x < *dimX; x++) {
-        for (y = 0; y < *dimY; y++) {
-            for (z = 0; z < *dimZ; z++) {
-                array3D[x * *dimY * *dimZ + y * *dimZ + z] = array3D[x * *dimY * *dimZ + y * *dimZ + z] + (unsigned char) (5 * randn(seed, 0));
-            }
-        }
-    }
-}
-
-/**
  * Fills a radius x radius matrix representing the disk
  * @param disk The pointer to the disk to be made
  * @param radius  The radius of the disk to be made
@@ -431,62 +289,6 @@ void strelDisk(int * disk, int radius) {
             double distance = sqrt(pow((double) (x - radius + 1), 2) + pow((double) (y - radius + 1), 2));
             if (distance < radius)
                 disk[x * diameter + y] = 1;
-        }
-    }
-}
-
-/**
- * Dilates the provided video
- * @param matrix The video to be dilated
- * @param posX The x location of the pixel to be dilated
- * @param posY The y location of the pixel to be dilated
- * @param poxZ The z location of the pixel to be dilated
- * @param dimX The x dimension of the frame
- * @param dimY The y dimension of the frame
- * @param dimZ The number of frames
- * @param error The error radius
- */
-void dilate_matrix(unsigned char * matrix, int posX, int posY, int posZ, int dimX, int dimY, int dimZ, int error) {
-    int startX = posX - error;
-    while (startX < 0)
-        startX++;
-    int startY = posY - error;
-    while (startY < 0)
-        startY++;
-    int endX = posX + error;
-    while (endX > dimX)
-        endX--;
-    int endY = posY + error;
-    while (endY > dimY)
-        endY--;
-    int x, y;
-    for (x = startX; x < endX; x++) {
-        for (y = startY; y < endY; y++) {
-            double distance = sqrt(pow((double) (x - posX), 2) + pow((double) (y - posY), 2));
-            if (distance < error)
-                matrix[x * dimY * dimZ + y * dimZ + posZ] = 1;
-        }
-    }
-}
-
-/**
- * Dilates the target matrix using the radius as a guide
- * @param matrix The reference matrix
- * @param dimX The x dimension of the video
- * @param dimY The y dimension of the video
- * @param dimZ The z dimension of the video
- * @param error The error radius to be dilated
- * @param newMatrix The target matrix
- */
-void imdilate_disk(unsigned char * matrix, int dimX, int dimY, int dimZ, int error, unsigned char * newMatrix) {
-    int x, y, z;
-    for (z = 0; z < dimZ; z++) {
-        for (x = 0; x < dimX; x++) {
-            for (y = 0; y < dimY; y++) {
-                if (matrix[x * dimY * dimZ + y * dimZ + z] == 1) {
-                    dilate_matrix(newMatrix, x, y, z, dimX, dimY, dimZ, error);
-                }
-            }
         }
     }
 }
@@ -512,81 +314,6 @@ void getneighbors(int * se, int numOnes, int * neighbors, int radius) {
             }
         }
     }
-}
-
-/**
- * The synthetic video sequence we will work with here is composed of a
- * single moving object, circular in shape (fixed radius)
- * The motion here is a linear motion
- * the foreground intensity and the background intensity is known
- * the image is corrupted with zero mean Gaussian noise
- * @param I The video itself
- * @param IszX The x dimension of the video
- * @param IszY The y dimension of the video
- * @param Nfr The number of frames of the video
- * @param seed The seed array used for number generation
- */
-void videoSequence(unsigned char * I, int IszX, int IszY, int Nfr, int * seed) {
-    int k;
-    int max_size = IszX * IszY * Nfr;
-    /*get object centers*/
-    int x0 = (int) roundDouble(IszY / 2.0);
-    int y0 = (int) roundDouble(IszX / 2.0);
-    I[x0 * IszY * Nfr + y0 * Nfr + 0] = 1;
-
-    /*move point*/
-    int xk, yk, pos;
-    for (k = 1; k < Nfr; k++) {
-        xk = abs(x0 + (k-1));
-        yk = abs(y0 - 2 * (k-1));
-        pos = yk * IszY * Nfr + xk * Nfr + k;
-        if (pos >= max_size)
-            pos = 0;
-        I[pos] = 1;
-    }
-
-    /*dilate matrix*/
-    unsigned char * newMatrix = (unsigned char *) malloc(sizeof (unsigned char) * IszX * IszY * Nfr);
-    imdilate_disk(I, IszX, IszY, Nfr, 5, newMatrix);
-    int x, y;
-    for (x = 0; x < IszX; x++) {
-        for (y = 0; y < IszY; y++) {
-            for (k = 0; k < Nfr; k++) {
-                I[x * IszY * Nfr + y * Nfr + k] = newMatrix[x * IszY * Nfr + y * Nfr + k];
-            }
-        }
-    }
-    free(newMatrix);
-
-    /*define background, add noise*/
-    setIf(0, 100, I, &IszX, &IszY, &Nfr);
-    setIf(1, 228, I, &IszX, &IszY, &Nfr);
-    /*add noise*/
-    addNoise(I, &IszX, &IszY, &Nfr, seed);
-
-}
-
-/**
- * Finds the first element in the CDF that is greater than or equal to the provided value and returns that index
- * @note This function uses sequential search
- * @param CDF The CDF
- * @param lengthCDF The length of CDF
- * @param value The value to be found
- * @return The index of value in the CDF; if value is never found, returns the last index
- */
-int findIndex(double * CDF, int lengthCDF, double value) {
-    int index = -1;
-    int x;
-    for (x = 0; x < lengthCDF; x++) {
-        if (CDF[x] >= value) {
-            index = x;
-            break;
-        }
-    }
-    if (index == -1) {
-        return lengthCDF - 1;
-    }
-    return index;
 }
 
 /**
@@ -683,15 +410,12 @@ void particleFilter(unsigned char * I, int IszX, int IszY, int Nfr, int * seed, 
 
     int indX, indY;
     //start send
-    long long send_start = get_time();
     check_error(cudaMemcpy(I_GPU, I, sizeof (unsigned char) *IszX * IszY*Nfr, cudaMemcpyHostToDevice));
     check_error(cudaMemcpy(objxy_GPU, objxy, sizeof (int) *2 * countOnes, cudaMemcpyHostToDevice));
     check_error(cudaMemcpy(weights_GPU, weights, sizeof (double) *Nparticles, cudaMemcpyHostToDevice));
     check_error(cudaMemcpy(xj_GPU, xj, sizeof (double) *Nparticles, cudaMemcpyHostToDevice));
     check_error(cudaMemcpy(yj_GPU, yj, sizeof (double) *Nparticles, cudaMemcpyHostToDevice));
     check_error(cudaMemcpy(seed_GPU, seed, sizeof (int) *Nparticles, cudaMemcpyHostToDevice));
-    long long send_end = get_time();
-    printf("TIME TO SEND TO GPU: %f\n", elapsed_time(send_start, send_end));
     int num_blocks = ceil((double) Nparticles / (double) threads_per_block);
 
 
@@ -718,8 +442,6 @@ void particleFilter(unsigned char * I, int IszX, int IszY, int Nfr, int * seed, 
 
 
     //block till kernels are finished
-    cudaThreadSynchronize();
-    long long back_time = get_time();
 
     cudaFree(xj_GPU);
     cudaFree(yj_GPU);
@@ -750,62 +472,34 @@ void particleFilter(unsigned char * I, int IszX, int IszY, int Nfr, int * seed, 
 
 int main(int argc, char * argv[]) {
 
-    char* usage = "double.out -x <dimX> -y <dimY> -z <Nfr> -np <Nparticles>";
-    //check number of arguments
-    if (argc != 9) {
-        printf("%s\n", usage);
-        return 0;
-    }
-    //check args deliminators
-    if (strcmp(argv[1], "-x") || strcmp(argv[3], "-y") || strcmp(argv[5], "-z") || strcmp(argv[7], "-np")) {
-        printf("%s\n", usage);
-        return 0;
-    }
-
     int IszX, IszY, Nfr, Nparticles;
 
     //converting a string to a integer
     if (sscanf(argv[2], "%d", &IszX) == EOF) {
-        printf("ERROR: dimX input is incorrect");
-        return 0;
     }
 
     if (IszX <= 0) {
-        printf("dimX must be > 0\n");
-        return 0;
     }
 
     //converting a string to a integer
     if (sscanf(argv[4], "%d", &IszY) == EOF) {
-        printf("ERROR: dimY input is incorrect");
-        return 0;
     }
 
     if (IszY <= 0) {
-        printf("dimY must be > 0\n");
-        return 0;
     }
 
     //converting a string to a integer
     if (sscanf(argv[6], "%d", &Nfr) == EOF) {
-        printf("ERROR: Number of frames input is incorrect");
-        return 0;
     }
 
     if (Nfr <= 0) {
-        printf("number of frames must be > 0\n");
-        return 0;
     }
 
     //converting a string to a integer
     if (sscanf(argv[8], "%d", &Nparticles) == EOF) {
-        printf("ERROR: Number of particles input is incorrect");
-        return 0;
     }
 
     if (Nparticles <= 0) {
-        printf("Number of particles must be > 0\n");
-        return 0;
     }
     //establish seed
     int * seed = (int *) malloc(sizeof (int) *Nparticles);
@@ -814,16 +508,10 @@ int main(int argc, char * argv[]) {
 	    seed[i] = i;
     //malloc matrix
     unsigned char * I = (unsigned char *) malloc(sizeof (unsigned char) *IszX * IszY * Nfr);
-    long long start = get_time();
     //call video sequence
     //videoSequence(I, IszX, IszY, Nfr, seed);
-    long long endVideoSequence = get_time();
-    printf("VIDEO SEQUENCE TOOK %f\n", elapsed_time(start, endVideoSequence));
     //call particle filter
     particleFilter(I, IszX, IszY, Nfr, seed, Nparticles);
-    long long endParticleFilter = get_time();
-    printf("PARTICLE FILTER TOOK %f\n", elapsed_time(endVideoSequence, endParticleFilter));
-    printf("ENTIRE PROGRAM TOOK %f\n", elapsed_time(start, endParticleFilter));
 
     free(seed);
     free(I);
