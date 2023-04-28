fcb8662e9c90fe18839285ab8a998b791d9b2ccd
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-04-28 19:00:40 +0900

    benchmarking script

--- scripts/run_all_gpu_benches.sh
+++ scripts/run_all_gpu_benches.sh
@@ -1,12 +1,64 @@
 #!/usr/bin/env bash
 
-HOST=memkf02
+# VALID_ARGS=$(getopt --long pgo_prof,pgo_opt,host,nruns,configs,clang,nvcc -- "$@")
+# if [[ $? -ne 0 ]]; then
+#     exit 1;
+# fi
 
-NRUNS=5
-
-#TEST_CONFIGS="0 2 3 4 102 103 104"
-#TEST_CONFIGS="0"
-TEST_CONFIGS="0 1 2 3 4 5"
+# eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    --pgo-alternatives)
+        echo "Will profile $2 alternatives"
+		PGO_ALTERNATIVE_NUM="$2"
+        shift 2
+        ;;
+    --pgo-prof)
+        echo "Will run PGO profiling on '$2'"
+		PGO_MODE="pgo_prof"
+		PGO_CONFIGS="$2"
+        shift 2
+        ;;
+    --pgo-opt)
+        echo "Will optimize '$2' using PGO"
+		PGO_MODE="pgo_opt"
+		PGO_CONFIGS="$2"
+        shift 2
+        ;;
+    --host)
+        echo "Will use '$2' host config"
+		HOST="$2"
+        shift 2
+        ;;
+    --nruns)
+        echo "Will run $2 times"
+		NRUNS="$2"
+        shift 2
+        ;;
+    --configs)
+        echo "Will run '$2' configs"
+		TEST_CONFIGS="$2"
+        shift 2
+        ;;
+    --clang)
+        echo "Will run clang"
+		TEST_CONFIGS="0 $TEST_CONFIGS"
+        shift
+        ;;
+    --nvcc)
+        echo "Will run nvcc"
+		RUN_NVCC=1
+        shift
+        ;;
+    --)
+		shift
+        break
+        ;;
+	*)
+		break
+		;;
+  esac
+done
 
 echo ----------------------------------------------
 echo Start "$(date -Ins)"
@@ -23,24 +75,46 @@ for i in $TEST_CONFIGS; do
 
 	echo Running polygeist configuration $i...
 	./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
-
 done
 
-echo Finished polygeist cuda "$(date -Ins)"
+if [ "$PGO_MODE" == "pgo_prof" ]; then
+   for i in $PGO_CONFIGS; do
+	   echo Compiling polygeist configuration $i for profiling...
+	   make cuda_clean &> /dev/null
+	   make POLYGEIST_ALTERNATIVES_MODE="$PGO_MODE" CONFIG="$i" TARGET_GPU=1 MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+	   ALTS=$(seq 0 $(($PGO_ALTERNATIVE_NUM - 1)))
+	   for j in $ALTS; do
+	       echo Profiling polygeist configuration $i alternative $j...
+	       POLYGEIST_PGO_KERNEL_ALTERNATIVE="$j" ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+	   done
+   done
+fi
 
+# if [ "$PGO_MODE" == "pgo_opt" ]; then
+#    for i in $PGO_CONFIGS; do
+# 	   echo Compiling polygeist configuration $i with PGO...
+# 	   make cuda_clean &> /dev/null
+# 	   make POLYGEIST_ALTERNATIVES_MODE="$PGO_MODE" CONFIG="$i" TARGET_GPU=1 MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+#
+# 	   echo Running polygeist configuration $i with PGO...
+# 	   ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+#    done
+# fi
 
-# NVCC
-./scripts/enable-config.sh common/host.make.config common/$HOST.nvcc.host.make.config
+echo Finished polygeist and clang cuda "$(date -Ins)"
 
-echo Compiling nvcc
-make cuda_clean &> /dev/null
-make MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+if [ "1" = "$RUN_NVCC" ]; then
+  ./scripts/enable-config.sh common/host.make.config common/$HOST.nvcc.host.make.config
 
-echo Running nvcc...
-./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+  echo Compiling nvcc
+  make cuda_clean &> /dev/null
+  make MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
 
-echo Finished nvcc cuda "$(date -Ins)"
-# NVCC END
+  echo Running nvcc...
+  ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+
+  echo Finished nvcc cuda "$(date -Ins)"
+fi
 
 
 
