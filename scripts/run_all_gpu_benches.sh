05f1b714a4d3a32f5894184fedcee86f782ac266
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-05-31 11:34:04 +0900

    Support for different targets in benchmarking script

--- scripts/run_all_gpu_benches.sh
+++ scripts/run_all_gpu_benches.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VALID_ARGS=$(getopt -o h --long dry-run,clang,nvcc,nruns:,pgo-prof-nruns:,pgo-alternatives:,pgo-prof:,pgo-opt:,host:,configs: -- "$@")
+VALID_ARGS=$(getopt -o h --long dry-run,targets:,clang,nvcc,nruns:,pgo-prof-nruns:,pgo-alternatives:,pgo-prof:,pgo-opt:,host:,configs: -- "$@")
 if [[ $? -ne 0 ]]; then
     exit 1;
 fi
@@ -12,75 +12,80 @@ PGO_PROF_NRUNS=3
 
 eval set -- "$VALID_ARGS"
 while [ : ]; do
-  case "$1" in
-    --pgo-alternatives)
-        echo "Profile $2 alternatives"
-		PGO_ALTERNATIVE_NUM="$2"
-        shift 2
-        ;;
-    --pgo-prof-nruns)
-        echo "Do $2 PGO profiling runs"
-		PGO_PROF_NRUNS="$2"
-        shift 2
-        ;;
-    --pgo-prof)
-        echo "Run PGO profiling on '$2'"
-		PGO_PROF="1"
-		PGO_PROF_CONFIGS="$2"
-        shift 2
-        ;;
-    --pgo-opt)
-        echo "Optimize '$2' using PGO"
-		PGO_OPT="1"
-		PGO_OPT_CONFIGS="$2"
-        shift 2
-        ;;
-    --host)
-        echo "Use '$2' host config"
-		HOST="$2"
-        shift 2
-        ;;
-    --nruns)
-        echo "Run $2 times"
-		NRUNS="$2"
-        shift 2
-        ;;
-    --configs)
-        echo "Run '$2' configs"
-		TEST_CONFIGS="$2"
-        shift 2
-        ;;
-    --dry-run)
-        echo "Dry run"
-		DRY_RUN="1"
-        shift
-        ;;
-    --clang)
-        echo "Run clang"
-		TEST_CONFIGS="0 $TEST_CONFIGS"
-        shift
-        ;;
-    --nvcc)
-        echo "Run nvcc"
-		RUN_NVCC=1
-        shift
-        ;;
-    -h)
-        echo "todo usage"
-        shift
-        ;;
-    --)
-		shift
-        break
-        ;;
-	*)
-		break
-		;;
-  esac
+    case "$1" in
+      --pgo-alternatives)
+          echo "Profile $2 alternatives"
+          PGO_ALTERNATIVE_NUM="$2"
+          shift 2
+          ;;
+      --pgo-prof-nruns)
+          echo "Do $2 PGO profiling runs"
+          PGO_PROF_NRUNS="$2"
+          shift 2
+          ;;
+      --targets)
+          echo "Targets: $2"
+          TARGETS="$2"
+          shift 2
+          ;;
+      --pgo-prof)
+          echo "Run PGO profiling on '$2'"
+          PGO_PROF="1"
+          PGO_PROF_CONFIGS="$2"
+          shift 2
+          ;;
+      --pgo-opt)
+          echo "Optimize '$2' using PGO"
+          PGO_OPT="1"
+          PGO_OPT_CONFIGS="$2"
+          shift 2
+          ;;
+      --host)
+          echo "Use '$2' host config"
+          HOST="$2"
+          shift 2
+          ;;
+      --nruns)
+          echo "Run $2 times"
+          NRUNS="$2"
+          shift 2
+          ;;
+      --configs)
+          echo "Run '$2' configs"
+          TEST_CONFIGS="$2"
+          shift 2
+          ;;
+      --dry-run)
+          echo "Dry run"
+          DRY_RUN="1"
+          shift
+          ;;
+      --clang)
+          echo "Run clang"
+          TEST_CONFIGS="0 $TEST_CONFIGS"
+          shift
+          ;;
+      --nvcc)
+          echo "Run nvcc"
+          RUN_NVCC=1
+          shift
+          ;;
+      -h)
+          echo "todo usage"
+          shift
+          ;;
+      --)
+          shift
+          break
+          ;;
+      *)
+          break
+          ;;
+    esac
 done
 
 if [ "$DRY_RUN" == "1" ]; then
-	exit
+    exit
 fi
 
 echo ----------------------------------------------
@@ -91,53 +96,63 @@ rm -r results/
 
 ./scripts/enable-config.sh common/host.make.config common/$HOST.polygeist.host.make.config
 
-for i in $TEST_CONFIGS; do
-	echo Compiling polygeist configuration $i...
-	make cuda_clean &> /dev/null
-	make CONFIG="$i" TARGET_GPU=1 MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
 
-	echo Running polygeist configuration $i...
-	./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+for target in $TARGETS; do
+    echo Starting target $target "$(date -Ins)"
+
+    for i in $TEST_CONFIGS; do
+        echo Compiling polygeist configuration $i...
+        make cuda_clean &> /dev/null
+        make CONFIG="$i" TARGET=$target MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+
+        echo Running polygeist configuration $i...
+        ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+    done
+
+    # TODO should we delete the PGO info somewhere?
+
+    if [ "$PGO_PROF" == "1" ]; then
+        for i in $PGO_PROF_CONFIGS; do
+            echo Compiling polygeist configuration $i for profiling...
+            make cuda_clean &> /dev/null
+            make POLYGEIST_ALTERNATIVES_MODE=pgo_prof CONFIG="$i" TARGET=$target MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+            ALTS=$(seq 0 $(($PGO_ALTERNATIVE_NUM - 1)))
+            for j in $ALTS; do
+                echo Profiling polygeist configuration $i alternative $j...
+                POLYGEIST_PGO_DATA_DIR="/var/tmp/polygeist/pgo/$i/" POLYGEIST_PGO_ALTERNATIVE="$j" \
+                    ./scripts/run_timed_cuda_big_n_times.sh $PGO_PROF_NRUNS 2>&1 | grep -B2 FAIL
+            done
+        done
+    fi
+
+    if [ "$PGO_OPT" == "1" ]; then
+        for i in $PGO_OPT_CONFIGS; do
+            echo Compiling polygeist configuration $i pgo...
+            make cuda_clean &> /dev/null
+            POLYGEIST_PGO_DATA_DIR="/var/tmp/polygeist/pgo/$i/" \
+                make POLYGEIST_ALTERNATIVES_MODE=pgo_opt CONFIG="$i" TARGET=$target MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+            echo Running polygeist configuration $i pgo...
+            ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+        done
+    fi
+
+    echo Finished target $target "$(date -Ins)"
+
 done
-
-if [ "$PGO_PROF" == "1" ]; then
-   for i in $PGO_PROF_CONFIGS; do
-	   echo Compiling polygeist configuration $i for profiling...
-	   make cuda_clean &> /dev/null
-	   make POLYGEIST_ALTERNATIVES_MODE=pgo_prof CONFIG="$i" TARGET_GPU=1 MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
-	   ALTS=$(seq 0 $(($PGO_ALTERNATIVE_NUM - 1)))
-	   for j in $ALTS; do
-	       echo Profiling polygeist configuration $i alternative $j...
-	       POLYGEIST_PGO_DATA_DIR="/var/tmp/polygeist/pgo/$i/" POLYGEIST_PGO_ALTERNATIVE="$j" \
-               ./scripts/run_timed_cuda_big_n_times.sh $PGO_PROF_NRUNS 2>&1 | grep -B2 FAIL
-	   done
-   done
-fi
-
-if [ "$PGO_OPT" == "1" ]; then
-   for i in $PGO_OPT_CONFIGS; do
-	   echo Compiling polygeist configuration $i pgo...
-	   make cuda_clean &> /dev/null
-	   POLYGEIST_PGO_DATA_DIR="/var/tmp/polygeist/pgo/$i/" \
-           make POLYGEIST_ALTERNATIVES_MODE=pgo_opt CONFIG="$i" TARGET_GPU=1 MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
-	   echo Running polygeist configuration $i pgo...
-	   ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
-   done
-fi
 
 echo Finished polygeist and clang cuda "$(date -Ins)"
 
 if [ "1" = "$RUN_NVCC" ]; then
-  ./scripts/enable-config.sh common/host.make.config common/$HOST.nvcc.host.make.config
+    ./scripts/enable-config.sh common/host.make.config common/$HOST.nvcc.host.make.config
 
-  echo Compiling nvcc
-  make cuda_clean &> /dev/null
-  make MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
+    echo Compiling nvcc
+    make cuda_clean &> /dev/null
+    make MY_VERIFICATION_DISABLE=1 cuda -kj &> /dev/null
 
-  echo Running nvcc...
-  ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
+    echo Running nvcc...
+    ./scripts/run_timed_cuda_big_n_times.sh $NRUNS 2>&1 | grep -B2 FAIL
 
-  echo Finished nvcc cuda "$(date -Ins)"
+    echo Finished nvcc cuda "$(date -Ins)"
 fi
 
 echo COPYING RESULTS, DONT QUIT
