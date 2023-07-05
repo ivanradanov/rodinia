6a4739af47ef8869c3888f2afe42f0501125d3d6
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-07-05 17:10:40 +0900

    add hecbench includes

---
+++ cuda/include/common.h
@@ -0,0 +1,18 @@
+#ifndef COMMON_
+#define COMMON_
+
+#include <CL/sycl.hpp>
+
+using namespace cl::sycl;
+constexpr access::mode sycl_read       = access::mode::read;
+constexpr access::mode sycl_write      = access::mode::write;
+constexpr access::mode sycl_read_write = access::mode::read_write;
+constexpr access::mode sycl_discard_read_write = access::mode::discard_read_write;
+constexpr access::mode sycl_discard_write = access::mode::discard_write;
+constexpr access::mode sycl_atomic     = access::mode::atomic;
+
+constexpr access::target sycl_cmem     = access::target::constant_buffer;
+constexpr access::target sycl_gmem     = access::target::global_buffer;
+constexpr access::target sycl_lmem     = access::target::local;
+
+#endif
